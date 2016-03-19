d6b957a6093a10055c686b30df94120c14da4af4
saptarshi <urinnersmile@gmail.com>
2016-03-19 16:31:28 -0700

    Bottom navigation layout and auto-hide for mobile

--- config.rb
+++ config.rb
@@ -6,14 +6,14 @@ require 'compass/import-once/activate'
 http_path = "../"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-
+output_style = :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Auto-show/hide bottom-nav and header
+
+var bottom_nav = document.querySelector(".bottom-nav");
+var headroom  = new Headroom(bottom_nav);
+headroom.init();
