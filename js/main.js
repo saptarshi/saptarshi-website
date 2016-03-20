8a306a25c43c8a3bb8ab6efb4d7129c0f76de9bf
saptarshi <urinnersmile@gmail.com>
2016-03-20 11:19:13 -0700

    Tuned scroll position calculation for sticky headers

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-var globals_not = {};
-// Auto-show/hide bottom-nav and header
-var bottom_nav = document.querySelector(".bottom-nav");
-var headroom = new Headroom(bottom_nav);
-headroom.init();
 
-//Initialize headers
-countHeaders();
+var globals_not = {};
+$(window).load(function(){
+  // Auto-show/hide bottom-nav and header
+  var bottom_nav = document.querySelector(".bottom-nav");
+  var headroom = new Headroom(bottom_nav);
+  headroom.init();
+
+  //Initialize headers
+  countHeaders();
+});
+
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
@@ -29,7 +33,7 @@ function debounce(func, wait, immediate) {
 function findQPosition(member, array) {
   var q_position = -1;
 
-  for(var i = array.length; i > 0 ; i--) {
+  for(var i = array.length; i >= 0 ; i--) {
     if(member > array[i-1]) {
       return q_position  = i;
     }
@@ -39,19 +43,28 @@ function findQPosition(member, array) {
 
 
 function countHeaders() {
-    var headers = $('.js-header');
+    var $headers = $('.js-header'),
+        $window = $(window),
+        $document = $(document),
+        sticked = [],
+        window_height = $window.height(),
+        scroll_top = $window.scrollTop(),
+        document_height = $document.height(),
+        dwh = document_height - window_height,
+        extra = (scroll_top > dwh) ? dwh - scroll_top : 0;
+
     var header_scroll_pos = [];
-    for(var i = 0; i< headers.length; i++) {
-      header_scroll_pos.push($(headers[i]).offset().top - 22);
+    for(var i = 0; i< $headers.length; i++) {
+      element_top = $($headers[i]).parent().offset().top,
+      header_scroll_pos.push(element_top - extra);
     }
     globals_not['header_scroll_pos'] = header_scroll_pos;
-    globals_not['headers'] = headers;
+    globals_not['headers'] = $headers;
 }
 var fixHeader = debounce(function() {
     var current_scroll = $(window).scrollTop(),
         q_pos = findQPosition(current_scroll, globals_not.header_scroll_pos);
     if(q_pos !== -1) {
-        console.log('Q position:' + q_pos);
         var org_name = $(globals_not.headers[q_pos-1]).find('.org-name').text(),
             org_year = $(globals_not.headers[q_pos-1]).find('.org-year').text();
         $('.floating-header .org-name').text(org_name);
@@ -60,6 +73,6 @@ var fixHeader = debounce(function() {
     } else {
       $('.floating-header').removeClass('appear');
     }
-}, 100);
+}, 250);
 
 $(window).on('scroll', fixHeader);
