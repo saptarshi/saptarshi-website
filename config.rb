f2b345155f2033745f5383a92f3c933893ffca29
saptarshi <urinnersmile@gmail.com>
2016-03-21 01:11:23 -0700

    Optimized for medium width devices, added project pages

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 require 'susy'
 require 'modular-scale'
+require 'breakpoint'
 
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
@@ -13,7 +14,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
